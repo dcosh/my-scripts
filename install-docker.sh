c42c7b89d4b2882bc54f8a64153da3f91709299d
Doug Cosh <fieldgeek2000@gmail.com>
2024-11-23 10:37:49 -0600

    modified:   install-docker.sh   removed progress checks

--- install-docker.sh
+++ install-docker.sh
@@ -12,19 +12,19 @@ echo "Add the repository to Apt sources:"
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/debian $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt-get update
 
-read -n 1 -p "All good? (Y/n)" $yn
+# read -n 1 -p "All good? (Y/n)" $yn
 
-# if [[$yn != "y"]] 
-# then [[$yn != "Y"]] 
-# then [[$yn != ""]] 
-# then exit 1
-# fi
+# # if [[$yn != "y"]] 
+# # then [[$yn != "Y"]] 
+# # then [[$yn != ""]] 
+# # then exit 1
+# # fi
 
-echo "continuing"
+#echo "continuing"
 
 sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
-read -n 1 -p "All good? (Y/n)" $yn
+#read -n 1 -p "All good? (Y/n)" $yn
 
 # if [[$yn != "y"]] 
 # then [[$yn != "Y"]] 
@@ -32,7 +32,7 @@ read -n 1 -p "All good? (Y/n)" $yn
 # then exit 1
 # fi
 
-echo "continuing"
-echo "testing docker..."
+#echo "continuing"
+#echo "testing docker..."
 
 sudo docker run hello-world
